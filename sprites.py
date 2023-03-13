948a26ed7e0fa58f0494ab06f273ac0ef756d160
David Jimenez <djimenez@murraystate.edu>
2023-03-13 19:59:25 -0400

    modify Mob class, add map class

--- sprites.py
+++ sprites.py
@@ -21,10 +21,10 @@ class Player(pygame.sprite.Sprite):
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
         self.rect.x += self.speedx
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
+        if self.rect.right > WIDTH - 64:
+            self.rect.right = WIDTH - 64
+        if self.rect.left < 64:
+            self.rect.left = 64
 
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
@@ -35,21 +35,34 @@ class Mob(pygame.sprite.Sprite):
     def __init__(self, game):
         pygame.sprite.Sprite.__init__(self)
         self.game = game
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image = pygame.transform.scale(ENEMY_IMG, (50, 50)).convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.mouthOpen = False
+        self.framesPassed = 0
+        self.rect.x = random.randrange(64, WIDTH - 64 - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 8)
-        self.speedx = random.randrange(-3, 3)
+        self.speedy = 2
 
     def update(self):
-        self.rect.x += self.speedx
+        self.framesPassed += 1
+        
+        if self.framesPassed % 10 == 0:
+            self.image = self.nextFrame(self.mouthOpen)
+
         self.rect.y += self.speedy
+
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.x = random.randrange(64, WIDTH - 64 - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 8)
+            self.speedy = 2
+
+    def nextFrame(self, mouthOpen):
+        self.mouthOpen = not mouthOpen
+
+        if mouthOpen:
+            return pygame.transform.scale( ENEMY_IMG_FRAME2, ( 50, 50 ) ).convert_alpha()
+        else:
+            return pygame.transform.scale( ENEMY_IMG, ( 50, 50 ) ).convert_alpha()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -66,3 +79,18 @@ class Bullet(pygame.sprite.Sprite):
         # kill if it moves off the top of the screen
         if self.rect.bottom < 0:
             self.kill()
+
+class Map(pygame.sprite.Sprite):
+    def __init__(self, game):
+        pygame.sprite.Sprite.__init__(self)
+        self.game = game
+        self.image = MAP.convert()
+        self.rect = self.image.get_rect()
+        self.rect.bottom = HEIGHT
+        self.rect.left = 0
+
+    def update(self):
+        self.rect.y += 1
+
+        if self.rect.top > -10:
+            self.rect.bottom = HEIGHT
